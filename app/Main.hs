44e7743d3ebf8741f7c41af073e0adaec5dd67ee
Kai-Oliver Prott <kai.prott@hotmail.de>
2020-07-17 17:41:30 +0200

    Modulation

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 import System.Environment
 import Network.HTTP.Simple
-import Data.ByteString.Lazy.UTF8 as BLU
+import qualified Data.ByteString.Lazy.UTF8 as BLU
 import Control.Exception
+
+import Debug.Trace
 
 main :: IO ()
 main = do
@@ -21,3 +23,37 @@ request url body = do
   case statuscode of
     "200" -> putStrLn ("Server response: " ++ BLU.toString (getResponseBody response))
     _     -> putStrLn ("Unexpected server response:\nHTTP code: " ++ statuscode)
+
+modulate :: Integer -> [Bool]
+modulate n =
+  sig ++
+  replicate len True ++ [False] ++
+  replicate (len * 4 - encLen) False ++ enc
+  where
+    sig | n >= 0    = [False, True]
+        | otherwise = [True, False]
+    enc = encodeWithLength (abs n)
+    encLen = length enc
+    len = (encLen + 3) `div` 4
+
+encodeWithLength :: Integer -> [Bool]
+encodeWithLength 0 = []
+encodeWithLength n = go n []
+    where
+      go 0 r = r
+      go k rs = go (div k 2) (odd k : rs)
+
+demodulate :: [Bool] -> Integer
+demodulate (s1 : s2 : bs) = sign s1 s2 * num
+  where
+    width = length $ takeWhile id bs
+    rest = take (4 * width) $ drop (width + 1) bs
+    num = foldl (\n b -> 2 * n + if b then 1 else 0) 0 rest
+
+sign :: Bool -> Bool -> Integer
+sign True False = -1
+sign False True = 1
+
+testModulation :: [Bool]
+testModulation =
+  map (\n -> n == demodulate (modulate n)) (concat [ [n, -n] | n <- [0..]])
