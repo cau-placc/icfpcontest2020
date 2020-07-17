b508b9213ab46e9e2ae5b8967471de68ff5ec154
Skgland <bb-github@t-online.de>
2020-07-17 19:49:31 +0200

    add new endpoint and move network do different module

--- app/Main.hs
+++ app/Main.hs
@@ -3,26 +3,20 @@ import Network.HTTP.Simple
 import qualified Data.ByteString.Lazy.UTF8 as BLU
 import Control.Exception
 
+import Network
+
 import Debug.Trace
 
 main :: IO ()
 main = do
   args <- getArgs
   putStrLn ("ServerUrl: " ++ head args ++ "; PlayerKey: " ++ args !! 1)
-  request (head args) (args !! 1) `catch` handler
+  request (head args) "" (args !! 1) `catch` handler
+  alienSend (head args) (args !! 1) `catch` handler
     where
         handler :: SomeException -> IO ()
         handler ex = putStrLn $ "Request failed with:\n" ++ show ex
 
-request :: String -> String -> IO ()
-request url body = do
-  request' <- parseRequest ("POST " ++ url)
-  let request = setRequestBodyLBS (BLU.fromString body) request'
-  response <- httpLBS request
-  let statuscode = show (getResponseStatusCode response)
-  case statuscode of
-    "200" -> putStrLn ("Server response: " ++ BLU.toString (getResponseBody response))
-    _     -> putStrLn ("Unexpected server response:\nHTTP code: " ++ statuscode)
 
 modulate :: Integer -> [Bool]
 modulate n =
---
+++ app/Network.hs
@@ -0,0 +1,14 @@
+
+request :: String -> String -> IO ()
+request url endpoint body = do
+  request' <- parseRequest ("POST " ++ url)
+  let request = setRequestBodyLBS (BLU.fromString body) request'
+  response <- httpLBS request
+  let statuscode = show (getResponseStatusCode response)
+  case statuscode of
+    "200" -> putStrLn ("Server response: " ++ BLU.toString (getResponseBody response))
+    _     -> putStrLn ("Unexpected server response:\nHTTP code: " ++ statuscode)
+
+
+alienSend :: String -> String -> IO ()
+alienSend server body = request server "/aliens/send" body
