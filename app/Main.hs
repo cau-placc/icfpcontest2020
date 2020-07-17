a014242a9af8b10d811df184802d4882459de950
Kai-Oliver Prott <kai.prott@hotmail.de>
2020-07-17 16:43:26 +0200

    Prettify code

--- app/Main.hs
+++ app/Main.hs
@@ -3,22 +3,21 @@ import Network.HTTP.Simple
 import Data.ByteString.Lazy.UTF8 as BLU
 import Control.Exception
 
-main = catch (
-    do
-        args <- getArgs
-        putStrLn ("ServerUrl: " ++ args!!0 ++ "; PlayerKey: " ++ args!!1)
-        request (args!!0) (args!!1)
-        request (args!!0) "21"
-    ) handler
+main :: IO ()
+main = do
+  args <- getArgs
+  putStrLn ("ServerUrl: " ++ head args ++ "; PlayerKey: " ++ args !! 1)
+  request (head args) (args !! 1) `catch` handler
     where
         handler :: SomeException -> IO ()
-        handler ex = putStrLn $ "Unexpected server response:\n" ++ show ex
+        handler ex = putStrLn $ "Request failed with:\n" ++ show ex
 
-request url body = do 
+request :: String -> String -> IO ()
+request url body = do
   request' <- parseRequest ("POST " ++ url)
   let request = setRequestBodyLBS (BLU.fromString body) request'
   response <- httpLBS request
   let statuscode = show (getResponseStatusCode response)
   case statuscode of
     "200" -> putStrLn ("Server response: " ++ BLU.toString (getResponseBody response))
-    _     -> putStrLn ("Unexpected server response:\nHTTP code: " ++ statuscode ++ "\nResponse body: " ++ BLU.toString (getResponseBody response))
+    _     -> putStrLn ("Unexpected server response:\nHTTP code: " ++ statuscode)
