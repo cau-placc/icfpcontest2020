7c83730fa0a9b981f587462aecaf033613f79463
Skgland <bb-github@t-online.de>
2020-07-17 16:02:12 +0200

    send a second message with halve th truth

--- app/Main.hs
+++ app/Main.hs
@@ -7,14 +7,18 @@ main = catch (
     do  
         args <- getArgs
         putStrLn ("ServerUrl: " ++ args!!0 ++ "; PlayerKey: " ++ args!!1)
-        request' <- parseRequest ("POST " ++ (args!!0))
-        let request = setRequestBodyLBS (BLU.fromString (args!!1)) request'
-        response <- httpLBS request
-        let statuscode = show (getResponseStatusCode response)
-        case statuscode of
-            "200" -> putStrLn ("Server response: " ++ BLU.toString (getResponseBody response))
-            _ -> putStrLn ("Unexpected server response:\nHTTP code: " ++ statuscode ++ "\nResponse body: " ++ BLU.toString (getResponseBody response))
+        request (args!!0) (args!!1)
+        request (args!!0) "21"
     ) handler
     where
         handler :: SomeException -> IO ()
         handler ex = putStrLn $ "Unexpected server response:\n" ++ show ex
+
+request url body = do 
+  request' <- parseRequest ("POST " ++ url)
+  let request = setRequestBodyLBS (BLU.fromString body) request'
+  response <- httpLBS request
+  let statuscode = show (getResponseStatusCode response)
+  case statuscode of
+    "200" -> putStrLn ("Server response: " ++ BLU.toString (getResponseBody response))
+    _     -> putStrLn ("Unexpected server response:\nHTTP code: " ++ statuscode ++ "\nResponse body: " ++ BLU.toString (getResponseBody response))
