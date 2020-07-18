a63b248cabdd122571519ba366e86a06b0e83e84
Skgland <bb-github@t-online.de>
2020-07-18 14:57:35 +0200

    fix []

--- app/Interpreter.hs
+++ app/Interpreter.hs
@@ -104,7 +104,7 @@ alienInteract = partial3 $ \prot state vec ->  runExpr $
 f38 = partial2 $ \prot state ->
   runExpr $
   app IF0 [ app Car [state]
-          , toExprList [app Modem [app Car [app Cdr [state]]], app MultipleDraw [app Car [app Cdr [app [Cdr state]]]]]
+          , toExprList [app Modem [app Car [app Cdr [state]]], app MultipleDraw [app Car [app Cdr [app Cdr [state]]]]]
           , app Interact [prot, app Modem [app Car [app Cdr [state]0]], app Send [app Car [app Cdr [app Cdr [state]]]]]]
 
 modem = partial1 $ \x -> runExpr $ app Dem [app Mod [x]]
