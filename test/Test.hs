5e6967e9606d376c6b85e0388156dae0fd4801cc
jhoefer <jonas.hoefer99@gmail.com>
2020-07-18 13:25:38 +0200

    Update test case

--- test/Test.hs
+++ test/Test.hs
@@ -9,6 +9,6 @@ main = do
     code       = unlines $ lines galaxy
     Right prog = parseAlienProg code
     Right (result, _) =
-      runMIB $ loadProg prog >> runExpr (Ident Galaxy)
-  putStrLn $ "\nResult: " <> show result
+      runMIB $ loadProg prog >> runExpr (App (App (App (Func Interact) (Ident Galaxy)) Nil) (App (App Cons (Number 0)) (Number 0)))
+  putStrLn $ "-----\nResult: " <> show result
 
