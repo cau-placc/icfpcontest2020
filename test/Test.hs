d1eb140667873b05b392881bc3c7fd5cc4030b6d
Skgland <bb-github@t-online.de>
2020-07-18 20:16:00 +0200

    use correct initial state

--- test/Test.hs
+++ test/Test.hs
@@ -11,6 +11,7 @@ statefullFile = "statefull.txt"
 emptyList = (Func Nil)
 list0 = app Cons [Number 0, emptyList]
 list1 = app Cons [Number 0, app Cons [Number 0]]
+tuple0 = app Cons [Number 0, Number 0]
 
 main :: IO ()
 main = do
@@ -18,7 +19,7 @@ main = do
   let code       = unlines $ lines galaxy
       Right prog = either (error . show) Right $ parseAlienProg code
       result     = runMIB $ loadProg prog >> runExpr
-        (app Interact [Ident Galaxy, list0, list1])
+        (app Interact [Ident Galaxy, emptyList, tuple0])
         >>= showData
   putStrLn $ "-----\nResult: " <> show result
 
