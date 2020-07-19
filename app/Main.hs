087616a27b512c8db57fa3377d5101437ac87f44
Skgland <bb-github@t-online.de>
2020-07-19 14:34:00 +0200

    instance FromValue for Commands

--- app/Main.hs
+++ app/Main.hs
@@ -217,9 +217,9 @@ instance FromValue Commands where
   fromValue v = do
     list <- fromValue v
     case list of
-      [Num 0, vector    ] -> (Accelerate $ ShipId - 1) <$> fromValue vector
-      [Num 1            ] -> pure $ Detonate $ ShipId -1
-      [Num 2, target, x3] -> (Shoot $ ShipId -1) <$> fromValue target <*> fromValue x3
+      [Num 0, vector    ] -> (Accelerate $ ShipId (-1)) <$> fromValue vector
+      [Num 1            ] -> pure $ Detonate $ ShipId (-1)
+      [Num 2, target, x3] -> (Shoot $ ShipId (-1)) <$> fromValue target <*> fromValue x3
       _                   -> Nothing
 
 
