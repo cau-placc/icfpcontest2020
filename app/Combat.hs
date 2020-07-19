74bbebe980e618651ead0c8343112260c3d3367f
Skgland <bb-github@t-online.de>
2020-07-19 22:49:42 +0200

    model shipConfig and add fork

--- app/Combat.hs
+++ app/Combat.hs
@@ -36,7 +36,7 @@ combat connection InvalidRequest = do
     state <-performAction "Nothing:  " $  doNothing connection
     combat connection state
 combat connection (GameResponse Waiting unknown state) = do
-    state <- performAction "Start:    " $ start connection (156,4,10,1)
+    state <- performAction "Start:    " $ start connection ShipConfig{fuel=300,x2=30,x3=10,x4=2}
     combat connection state
 combat connection (GameResponse Running unknown (Just state)) = do
     let Unknown _ role _ _ _ = unknown
@@ -62,9 +62,9 @@ join connection@(Connection _ playerKey _) = let
     in
       postAlien connection body
 
-start :: Connection -> ShipConfiguration-> IO (Either StatusCode ResponseBody)
-start connection@(Connection _ playerKey _) (n1,n2,n3,n4) = let
-      body = modulateValue $ toValue [Num 3, Num playerKey, toValue [Num n1, Num n2, Num n3, Num n4]]
+start :: Connection -> ShipConfig -> IO (Either StatusCode ResponseBody)
+start connection@(Connection _ playerKey _) ShipConfig{fuel = fuel,x2 = x2, x3 = x3, x4 = x4} = let
+      body = modulateValue $ toValue [Num 3, Num playerKey, toValue [Num fuel, Num x2, Num x3, Num x4]]
     in
       postAlien connection body
 
@@ -91,22 +91,28 @@ accelerate connection shipId vector = command connection [Accelerate shipId vect
 createCommandFor :: Role -> Tick -> [(ShipState, [ReceivedCommand])] -> (ShipState, [ReceivedCommand]) -> [SendCommand]
 createCommandFor ourrole tick allShips
   (ShipState role idt (Position (Vector x y))
-                      (Velocity (Vector xd yd)) _ _ _ _, _)
+                      (Velocity (Vector xd yd)) ShipConfig{x4 = x4} x2 x3 x4, _)
   | ourrole == role =
     trace ("Predicted: " ++ show predictedPos ++ "; Wanted: " ++ show wantedPos ++
-           "Shot at: "   ++ show (tpx, tpy))
-    [ Accelerate idt (Vector accX accY)
-    , Shoot      idt (Vector tpx  tpy ) 5
-    ]
+           "Shot at: "   ++ show (tpx, tpy)) $
+            if x4 <= 1 then
+              [ Accelerate idt (Vector accX accY)
+              , Shoot      idt (Vector tpx  tpy ) 5
+              ]
+            else
+              [ Accelerate idt (Vector accX accY)
+              , Fork       idt []
+              ]
   | otherwise       = []
   where
     (accX, accY) = predictedPos - wantedPos
-    wantedPos    = (x, y) + (scale 2 $ rotate (getGravOffestFor (x, y)))
+    wantedPos    = (x, y) + (rotate (getGravOffestFor (x, y)))
     predictedPos = (x, y) + getGravOffestFor (x, y) + (xd, yd)
     (ShipState _ _ (Position (Vector tx ty))
                    (Velocity (Vector txd tyd)) _ _ _ _, _) =
       getOtherShip role allShips
     (tpx, tpy) = (tx, ty) + getGravOffestFor (tx, ty) + (txd, tyd)
+
 
 scale :: Integer -> (Integer, Integer) -> (Integer , Integer)
 scale r (x,y) = (x*r, y*r)
@@ -141,12 +147,12 @@ showDemodulated = show . demodulateValue
 demodulateResponse :: String -> Maybe GameResponse
 demodulateResponse = fromValue . demodulateValue
 
-type ShipConfiguration = (Integer,Integer,Integer,Integer)
 data Status = Waiting |  Running | Done deriving Show
 
 data SendCommand  = Accelerate ShipId Vector
                   | Detonate ShipId
                   | Shoot ShipId Vector Integer
+                  | Fork [Value] -- the parameters if any of fork are unknown
                   deriving Show
 
 data ReceivedCommand = Accelerated Vector
@@ -164,8 +170,10 @@ data ShipId   = ShipId   Integer  deriving Show
 data Position = Position Vector   deriving Show
 data Velocity = Velocity Vector   deriving Show
 
-data ShipState = ShipState Role ShipId Position Velocity Value Value Value Value  deriving Show
-data GameState = GameState Tick Value [(ShipState, [ReceivedCommand])]             deriving Show
+data ShipConfig = ShipConfig { fuel::Integer, x2 :: Integer, x3::Integer, x4 :: Integer}
+
+data ShipState = ShipState Role ShipId Position Velocity ShipConfig Value Value Value  deriving Show
+data GameState = GameState Tick Value [(ShipState, [ReceivedCommand])]                 deriving Show
 
 class FromValue a where
   fromValue :: Value -> Maybe a
@@ -308,6 +316,7 @@ instance ToValue SendCommand where
   toValue (Accelerate shipId vector   ) = toValue (0::Integer, shipId, vector)
   toValue (Detonate   shipId          ) = toValue (1::Integer, shipId)
   toValue (Shoot      shipId target x3) = traceShowId $ toValue (2::Integer, shipId, target, x3)
+  toValue (Fork       shipId params   ) = toValue (toValue 3::Integer : toValue shipId : params )
 
 instance ToValue ShipId where
   toValue (ShipId i) = toValue i
